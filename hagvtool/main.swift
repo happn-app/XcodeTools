e08375dfe87fb9ab0fba6ceb220c2839f9440359
Frizlab <fload@me.com>
2016-12-13 11:10:10 +0100

    Initial Commit

---
+++ hagvtool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hagvtool
+//
+//  Created by François Lamboley on 12/13/16.
+//  Copyright © 2016 happn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
