a27db338f4cc8ba3f9ae41fe5ad5eafea3f6f23d
Frizlab <fload@me.com>
2017-01-01 15:23:30 +0100

    Move log type to global CLI arg

--- hagvtool/main.swift
+++ hagvtool/main.swift
@@ -119,56 +119,62 @@ func usage<TargetStream: TextOutputStream>(program_name: String, stream: inout T
 	print("hagvtool - happn agvtool", to: &stream)
 	print("The goal of the project is to provide agvtool with support for targets in a project. The CVS part of agvtool has been completly dropped.", to: &stream)
 	print("", to: &stream)
-	print("Usage: \(program_name) [--project-path=xcodeproj_path] [--targets=target_name_1,target_name_2,...] command [command_args]", to: &stream)
+	print("Usage: \(program_name) [--project-path=xcodeproj_path] [--targets=target_name_1,target_name_2,...] [--porcelain|--quiet] command [command_args]", to: &stream)
 	print("When a target name is given, regular expression are supported.", to: &stream)
 	print("", to: &stream)
 	print("Commands are:", to: &stream)
 	print("   help", to: &stream)
 	print("      Outputs this help", to: &stream)
 	print("", to: &stream)
-	print("   print-build-number | what-version | vers [--error-on-no-apple-versioning] [--error-on-no-plist-version] [--porcelain]", to: &stream)
+	print("   print-build-number | what-version | vers [--error-on-no-apple-versioning] [--error-on-no-plist-version] ", to: &stream)
 	print("      Outputs the build numbers of the given targets, or all the targets if none are specified", to: &stream)
 	print("", to: &stream)
-	print("   bump-build-number | next-version | bump [--force-apple-versioning] [--force-plist-versioning] [--porcelain|--quiet]", to: &stream)
+	print("   bump-build-number | next-version | bump [--force-apple-versioning] [--force-plist-versioning]", to: &stream)
 	print("      Bump the build numbers of the given targets, or all the targets if none are specified and outputs the new build numbers", to: &stream)
 	print("", to: &stream)
-	print("   set-build-number | new-version [--force-apple-versioning] [--force-plist-versioning] [--porcelain|--quiet] new_build_number", to: &stream)
+	print("   set-build-number | new-version [--force-apple-versioning] [--force-plist-versioning] new_build_number", to: &stream)
 	print("      Set the build numbers of the given targets, or all the targets if none are specified and outputs the new build numbers", to: &stream)
 	print("", to: &stream)
-	print("   print-marketing-version | what-marketing-version | mvers [--porcelain]", to: &stream)
+	print("   print-marketing-version | what-marketing-version | mvers", to: &stream)
 	print("      Outputs the marketing versions of the given targets, or all the targets if none are specified", to: &stream)
 	print("", to: &stream)
-	print("   set-marketing-version | new-marketing-version [--porcelain|--quiet] new_marketing_version", to: &stream)
+	print("   set-marketing-version | new-marketing-version new_marketing_version", to: &stream)
 	print("      Set the marketing versions of the given targets, or all the targets if none are specified and outputs the new marketing numbers", to: &stream)
 	print("", to: &stream)
-	print("   print-swift-code [--porcelain]", to: &stream)
+	print("   print-swift-code", to: &stream)
 	print("      Outputs the Swift code you’ll have to use in order to access the version of the given targets in a Swift project", to: &stream)
 }
 
 /* Returns the arg at the given index, or prints "Syntax error: error_message"
  * and the usage, then exits with syntax error if there is not enough arguments
  * given to the program */
-func argAtIndexOrExit(_ i: Int, error_message: String) -> String {
+func argAtIndexOrExit(_ i: inout Int, error_message: String) -> String {
 	guard CommandLine.arguments.count > i else {
 		print("Syntax error: \(error_message)", to: &mx_stderr)
 		usage(program_name: CommandLine.arguments[0], stream: &mx_stderr)
 		exit(1)
 	}
 	
-	return CommandLine.arguments[i]
+	i += 1
+	return CommandLine.arguments[i-1]
 }
 
-func argAtIndex(_ i: Int) -> String? {
+func argAtIndex(_ i: inout Int) -> String? {
 	guard CommandLine.arguments.count > i else {return nil}
-	return CommandLine.arguments[i]
+	
+	i += 1
+	return CommandLine.arguments[i-1]
+}
+
+func getLongArgValue(fromGetLongArgsValue: String?, argIdx: inout Int) -> String {
+	if let v = fromGetLongArgsValue {return v}
+	return argAtIndexOrExit(&argIdx, error_message: "Cannot find value for long arg.")
 }
 
 /* Takes the current arg position in input and a dictionary of long args names
  * with the corresponding action to execute when the long arg is found.
  * Returns the new arg position when all long args have been found. */
-func getLongArgs(argIdx: Int, expectsMore: Bool = true, longArgs: [String: (String) -> Void]) -> Int {
-	var i = argIdx
-	
+func getLongArgs(argIdx: inout Int, longArgs: [String: (_ curArgPos: /* :( inout does not work here (Swift 3.0.1) */Int, _ argValue: String?) -> Int]) {
 	func stringByDeletingPrefixIfPresent(_ prefix: String, from string: String) -> String? {
 		if string.hasPrefix(prefix) {
 			return string[string.index(string.startIndex, offsetBy: prefix.characters.count)..<string.endIndex]
@@ -179,35 +185,36 @@ func getLongArgs(argIdx: Int, expectsMore: Bool = true, longArgs: [String: (Stri
 	
 	
 	longArgLoop: while true {
-		let arg: String
-		if expectsMore {arg = argAtIndexOrExit(i, error_message: "expected more argument")}
-		else {
-			guard let a = argAtIndex(i) else {return i}
-			arg = a
-		}
-		i += 1
+		guard let arg = argAtIndex(&argIdx) else {return}
 		
 		for (longArg, action) in longArgs {
 			if let no_prefix = stringByDeletingPrefixIfPresent("--\(longArg)=", from: arg) {
-				action(no_prefix)
+				argIdx = action(argIdx, no_prefix)
 				continue longArgLoop
 			} else if "--" + longArg == arg {
-				action("") /* Yes. I know. */
+				argIdx = action(argIdx, nil)
 				continue longArgLoop
 			}
 		}
 		
-		if arg != "--" {i -= 1}
+		if arg != "--" {argIdx -= 1}
 		break
 	}
-	
-	return i
 }
 
 
 /* ***************
    MARK: - Objects
    *************** */
+
+enum LogType {
+	
+	case quiet
+	case humanReadable
+	case porcelain
+	case json /* Not implemented */
+	
+}
 
 struct BuildConfig {
 	
@@ -439,9 +446,12 @@ var curArgIdx = 1
 
 var project_path_arg: String?
 var targets_criteria_arg: String?
-curArgIdx = getLongArgs(argIdx: curArgIdx, longArgs: [
-		"project-path": { project_path_arg = $0 },
-		"targets": { targets_criteria_arg = $0 }
+var logType = LogType.humanReadable
+getLongArgs(argIdx: &curArgIdx, longArgs: [
+		"project-path": { var i = $0; project_path_arg = getLongArgValue(fromGetLongArgsValue: $1, argIdx: &i); return i },
+		"targets": { var i = $0; targets_criteria_arg = getLongArgValue(fromGetLongArgsValue: $1, argIdx: &i); return i },
+		"porcelain": { logType = .porcelain; return $0.0 },
+		"quiet": { logType = .quiet; return $0.0 }
 	]
 )
 
@@ -639,24 +649,47 @@ for target_ref in target_refs {
 	targets.append(target)
 }
 
+/* ***************************
+   MARK: - Showing Common Info
+   *************************** */
+
+switch logType {
+case .quiet:
+	()
+	
+case .porcelain:
+	print("\(pbxproj_url.path)")
+	
+	var first = true
+	for target in targets {
+		if !first {print(",", terminator: "")}
+		print(target.name.safeString(forChars: ","), terminator: "")
+		first = false
+	}
+	print()
+	
+case .humanReadable:
+	print("Using pbxproj file at \(pbxproj_url.path)")
+	print("Found \(targets.count) target\(targets.count != 1 ? "s" : "") matching the given criteria")
+	
+case .json:
+	fatalError("Not implemented")
+}
 
 /* ************************
    MARK: - Applying Command
    ************************ */
 
-curArgIdx += 1
-switch argAtIndexOrExit(curArgIdx-1, error_message: "Command is required") {
+switch argAtIndexOrExit(&curArgIdx, error_message: "Command is required") {
 case "help":
 	usage(program_name: CommandLine.arguments[0], stream: &mx_stdout)
 	
 case "print-build-number", "what-version", "vers":
-	var porcelain = false
 	var errOnNoPlistVersion = false
 	var errOnNoAppleVersioning = false
-	curArgIdx = getLongArgs(argIdx: curArgIdx, expectsMore: false, longArgs: [
-			"porcelain": { _ in porcelain = true },
-			"error-on-no-plist-version": { _ in errOnNoPlistVersion = true },
-			"error-on-no-apple-versioning": { _ in errOnNoAppleVersioning = true }
+	getLongArgs(argIdx: &curArgIdx, longArgs: [
+			"error-on-no-plist-version": { errOnNoPlistVersion = true; return $0.0 },
+			"error-on-no-apple-versioning": { errOnNoAppleVersioning = true; return $0.0 }
 		]
 	)
 	let misconfigsMask = BuildConfig.Misconfigs(
@@ -666,26 +699,11 @@ case "print-build-number", "what-version", "vers":
 		diffBuildNumbers: (projectConf: "", infoPlist: "")
 	)
 	
-	if porcelain {
-		print("\(pbxproj_url.path)")
-		
-		var first = true
-		for target in targets {
-			if !first {print(",", terminator: "")}
-			print(target.name.safeString(forChars: ","), terminator: "")
-			first = false
-		}
-		print()
-	} else {
-		print("Using pbxproj file at \(pbxproj_url.path)")
-		print("Found \(targets.count) target\(targets.count != 1 ? "s" : "") matching the given criteria")
-	}
-	
 	/* Showing errors if any */
 	let n = targets.reduce(0) { $0 + ($1.distinctActualMisconfigurations(matchingMisconfigsMask: misconfigsMask).isEmpty ? 0 : 1) }
 	let a = targets.reduce(true) { $0 && $1.doAllOrNoneOfBuildConfigsHaveMisconfigsMask(misconfigsMask) }
 	if n > 0 {
-		if !porcelain {
+		if .humanReadable ~= logType {
 			print("\(n) target\(n != 1 ? "s are" : " is") misconfigured in \(a ? "all" : "some") of \(n != 1 ? "their" : "its") build configurations:", to: &mx_stderr)
 		}
 		let buildNumbersDifferMask = BuildConfig.Misconfigs(noInfoPlist: false, unreadablePlistPath: nil, noBuildNumberInPlist: false, noMarketingNumberInPlist: false, noAppleVersioning: false, diffBuildNumbers: (projectConf: "", infoPlist: ""))
@@ -697,60 +715,65 @@ case "print-build-number", "what-version", "vers":
 			do {
 				/* Showing build numbers differ error */
 				let buildConfigs = target.misconfiguredBuildConfigs(matchingMisconfigsMask: buildNumbersDifferMask)
-				let showAll = porcelain || buildConfigs.count != target.buildConfigs.count
+				let showAll = (.porcelain ~= logType || .json ~= logType) || buildConfigs.count != target.buildConfigs.count
 				for buildConfig in (showAll ? buildConfigs : [buildConfigs[0]]) {
-					if porcelain {
-						print("cfg_err:diff_build_number \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
-					} else {
-						print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" build numbers differ in project conf and Info.plist (when bumping build number, the Info.plist version will be used, unless only --force-apple-versioning is set)", to: &mx_stderr)
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("cfg_err:diff_build_number \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
+					case .humanReadable: print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" build numbers differ in project conf and Info.plist (when bumping build number, the Info.plist version will be used, unless only --force-apple-versioning is set)", to: &mx_stderr)
+					case .json: fatalError("Not implemented")
 					}
 				}
 			}
 			do {
 				/* Showing unreadable Info.plist error */
 				let buildConfigs = target.misconfiguredBuildConfigs(matchingMisconfigsMask: unreadablePlistMask)
-				let showAll = porcelain || buildConfigs.count != target.buildConfigs.count
+				let showAll = (.porcelain ~= logType || .json ~= logType) || buildConfigs.count != target.buildConfigs.count
 				for buildConfig in (showAll ? buildConfigs : [buildConfigs[0]]) {
-					if porcelain {
-						print("err:unreadable_plist \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))/\(buildConfig.misconfigs.unreadablePlistPath!.safeString(forChars: "/"))", to: &mx_stderr)
-					} else {
-						print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" Info.plist file not found or unreadable (path \(buildConfig.misconfigs.unreadablePlistPath!))", to: &mx_stderr)
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("err:unreadable_plist \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))/\(buildConfig.misconfigs.unreadablePlistPath!.safeString(forChars: "/"))", to: &mx_stderr)
+					case .humanReadable: print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" Info.plist file not found or unreadable (path \(buildConfig.misconfigs.unreadablePlistPath!))", to: &mx_stderr)
+					case .json: fatalError("Not implemented")
 					}
 				}
 			}
 			if errOnNoAppleVersioning {
 				/* Showing target not configured for Apple Versioning error */
 				let buildConfigs = target.misconfiguredBuildConfigs(matchingMisconfigsMask: noAppleVersioningMask)
-				let showAll = porcelain || buildConfigs.count != target.buildConfigs.count
+				let showAll = (.porcelain ~= logType || .json ~= logType) || buildConfigs.count != target.buildConfigs.count
 				for buildConfig in (showAll ? buildConfigs : [buildConfigs[0]]) {
-					if porcelain {
-						print("cfg_err:no_apple_version \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
-					} else {
-						print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" is not configured to use apple-versioning, or version in project conf is empty (fix with bump-build-number or set-build-number --force-apple-versioning)", to: &mx_stderr)
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("cfg_err:no_apple_version \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
+					case .humanReadable: print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" is not configured to use apple-versioning, or version in project conf is empty (fix with bump-build-number or set-build-number --force-apple-versioning)", to: &mx_stderr)
+					case .json: fatalError("Not implemented")
 					}
 				}
 			}
 			if errOnNoPlistVersion {
 				/* Showing no build number in plist error */
 				let buildConfigs = target.misconfiguredBuildConfigs(matchingMisconfigsMask: noPlistVersionMask)
-				let showAll = porcelain || buildConfigs.count != target.buildConfigs.count
+				let showAll = (.porcelain ~= logType || .json ~= logType) || buildConfigs.count != target.buildConfigs.count
 				for buildConfig in (showAll ? buildConfigs : [buildConfigs[0]]) {
-					if porcelain {
-						print("cfg_err:no_plist_build_number \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
-					} else {
-						print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" have an Info.plist, but no build number in the plist (fix with bump-build-number or set-build-number --force-plist-versioning)", to: &mx_stderr)
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("cfg_err:no_plist_build_number \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
+					case .humanReadable: print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" have an Info.plist, but no build number in the plist (fix with bump-build-number or set-build-number --force-plist-versioning)", to: &mx_stderr)
+					case .json: fatalError("Not implemented")
 					}
 				}
 			}
 			if errOnNoPlistVersion {
 				/* Showing no plist error */
 				let buildConfigs = target.misconfiguredBuildConfigs(matchingMisconfigsMask: noPlistMask)
-				let showAll = porcelain || buildConfigs.count != target.buildConfigs.count
+				let showAll = (.porcelain ~= logType || .json ~= logType) || buildConfigs.count != target.buildConfigs.count
 				for buildConfig in (showAll ? buildConfigs : [buildConfigs[0]]) {
-					if porcelain {
-						print("cfg_err:no_plist \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
-					} else {
-						print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" does not have an Info.plist (you'll have to manually add an Info.plist file to fix this problem)", to: &mx_stderr)
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("cfg_err:no_plist \(target.name.safeString(forChars: "/"))/\(buildConfig.name.safeString(forChars: "/"))", to: &mx_stderr)
+					case .humanReadable: print("   - \"\(target.name)\(showAll ? "/" + buildConfig.name : "")\" does not have an Info.plist (you'll have to manually add an Info.plist file to fix this problem)", to: &mx_stderr)
+					case .json: fatalError("Not implemented")
 					}
 				}
 			}
@@ -780,33 +803,36 @@ case "print-build-number", "what-version", "vers":
 	if !diffVersions {
 		assert(!diffVersionsInOneTarget)
 		if let version = version {
-			if porcelain {
-				print(":\(version)")
-			} else {
-				print("All targets and configurations are setup with build number \(version)")
+			switch logType {
+			case .quiet: (/*nop*/)
+			case .porcelain:     print(":\(version)")
+			case .humanReadable: print("All targets and configurations are setup with build number \(version)")
+			case .json: fatalError("Not implemented")
 			}
 		}
 	} else {
-		if !porcelain {
+		if .humanReadable ~= logType {
 			print("Build numbers by targets\(diffVersionsInOneTarget ? " and configurations" : ""):")
 		}
 		for target in targets {
 			let distinctVersions = target.distinctBuildNumbers
 			if distinctVersions.count == 1 {
 				if let v = distinctVersions[0] {
-					if porcelain {
-						print("/\(target.name.safeString(forChars: ":")):\(v)")
-					} else {
-						print("   - \(target.name): \(v)")
+					switch logType {
+					case .quiet: (/*nop*/)
+					case .porcelain:     print("/\(target.name.safeString(forChars: ":")):\(v)")
+					case .humanReadable: print("   - \(target.name): \(v)")
+					case .json: fatalError("Not implemented")
 					}
 				}
 			} else {
 				for buildConfig in target.buildConfigs {
 					if let v = buildConfig.fullBuildNumber.reduced() {
-						if porcelain {
-							print("|\(buildConfig.name.safeString(forChars: "/"))/\(target.name.safeString(forChars: ":")):\(v)")
-						} else {
-							print("   - \(target.name)/\(buildConfig.name): \(v)")
+						switch logType {
+						case .quiet: (/*nop*/)
+						case .porcelain:     print("|\(buildConfig.name.safeString(forChars: "/"))/\(target.name.safeString(forChars: ":")):\(v)")
+						case .humanReadable: print("   - \(target.name)/\(buildConfig.name): \(v)")
+						case .json: fatalError("Not implemented")
 						}
 					}
 				}
