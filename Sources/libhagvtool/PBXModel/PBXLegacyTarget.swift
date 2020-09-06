40cfb3da18b76650bb244e136f404c9e81a83d0b
Frizlab <fload@me.com>
2020-09-06 14:16:14 +0200

    Add missing entities from http://www.monobjc.net/xcode-project-file-format.html

---
+++ Sources/libhagvtool/PBXModel/PBXLegacyTarget.swift
@@ -0,0 +1,14 @@
+import CoreData
+import Foundation
+
+
+
+/* From http://www.monobjc.net/xcode-project-file-format.html */
+@objc(PBXLegacyTarget)
+public class PBXLegacyTarget : PBXTarget {
+	
+	open override func fillValues(rawObject: [String : Any], rawObjects: [String : [String : Any]], context: NSManagedObjectContext, decodedObjects: inout [String : PBXObject]) throws {
+		try super.fillValues(rawObject: rawObject, rawObjects: rawObjects, context: context, decodedObjects: &decodedObjects)
+	}
+	
+}
