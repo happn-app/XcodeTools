3c2f3c4532afdf8a506bb54c7eeeac6cd77554f3
Frizlab <fload@me.com>
2020-09-12 14:46:21 +0200

    Add a test project

---
+++ TestsData/project1/target1/AppDelegate.swift
@@ -0,0 +1,24 @@
+/*
+ * AppDelegate.swift
+ * Target 1
+ *
+ * Created by François Lamboley on 11/09/2020.
+ */
+
+import Cocoa
+
+
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+	
+	@IBOutlet var window: NSWindow!
+	
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+	}
+	
+	func applicationWillTerminate(_ aNotification: Notification) {
+	}
+	
+}
