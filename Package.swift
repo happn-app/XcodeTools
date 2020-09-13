66147b909bc7910773c33da67551d5b65b286d10
Frizlab <fload@me.com>
2020-09-13 16:39:08 +0200

    Clear things up

--- Package.swift
+++ Package.swift
@@ -3,15 +3,24 @@ import PackageDescription
 
 
 let package = Package(
-	name: "XcodeProjKit",
+	name: "XcodeTools",
 	platforms: [
 		.macOS(.v10_15)
 	],
 	products: [
 		.library(name: "XcodeProjKit", targets: ["XcodeProjKit"]),
+		.executable(name: "hagvtool", targets: ["hagvtool"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.1")
 	],
 	targets: [
 		.target(name: "XcodeProjKit", dependencies: [], resources: [Resource.process("PBXModel.xcdatamodeld")]),
-		.testTarget(name: "XcodeProjKitTests", dependencies: ["XcodeProjKit"])
+		.testTarget(name: "XcodeProjKitTests", dependencies: ["XcodeProjKit"]),
+		
+		.target(name: "hagvtool", dependencies: [
+			.product(name: "ArgumentParser", package: "swift-argument-parser"),
+			"XcodeProjKit"
+		])
 	]
 )
