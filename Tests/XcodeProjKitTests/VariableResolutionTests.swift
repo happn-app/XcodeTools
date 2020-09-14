7e8af9adc3a92f7ca87e97038ab602241a52e52f
Frizlab <fload@me.com>
2020-09-14 09:04:52 +0200

    Add a few tests

--- Tests/XcodeProjKitTests/VariableResolutionTests.swift
+++ Tests/XcodeProjKitTests/VariableResolutionTests.swift
@@ -7,17 +7,42 @@ import XCTest
 
 class VariableResolutionTests : XCTestCase {
 	
-	let refSettings = CombinedBuildSettings(targetName: nil, configurationName: "Test", buildSettingsLevels: [BuildSettings(rawBuildSettings: ["VALUE": "Value"])])
+	let refSettings = CombinedBuildSettings(targetName: nil, configurationName: "Test", buildSettingsLevels: [
+		BuildSettings(rawBuildSettings: ["VALUE_1": "Value 1"]),
+		BuildSettings(rawBuildSettings: ["VALUE_2": "Value 2"]),
+		BuildSettings(rawBuildSettings: ["VALUE_12": "Value 12"]),
+		BuildSettings(rawBuildSettings: ["VAR": "NUMBER2"]),
+		BuildSettings(rawBuildSettings: ["NUMBER1": "1"]),
+		BuildSettings(rawBuildSettings: ["NUMBER2": "2"])
+	])
 	
 	func testSimpleResolutionWithParenthesis() throws {
-		let actual = refSettings.resolveVariables(in: "prefix $(VALUE) suffix")
-		let expected = "prefix Value suffix"
+		let actual = refSettings.resolveVariables(in: "prefix $(VALUE_1) suffix")
+		let expected = "prefix Value 1 suffix"
+		XCTAssertEqual(actual, expected)
+	}
+	
+	func testEmbeddedOnceResolution() throws {
+		let actual = refSettings.resolveVariables(in: "prefix $(VALUE_$(NUMBER2)) suffix")
+		let expected = "prefix Value 2 suffix"
+		XCTAssertEqual(actual, expected)
+	}
+	
+	func testEmbeddedOnceTwoTimesResolution() throws {
+		let actual = refSettings.resolveVariables(in: "prefix $(VALUE_$(NUMBER1)$(NUMBER2)) suffix")
+		let expected = "prefix Value 12 suffix"
+		XCTAssertEqual(actual, expected)
+	}
+	
+	func testEmbeddedTwiceResolution() throws {
+		let actual = refSettings.resolveVariables(in: "prefix $(VALUE_$($(VAR))) suffix")
+		let expected = "prefix Value 2 suffix"
 		XCTAssertEqual(actual, expected)
 	}
 	
 	func testSimpleResolutionWithoutParenthesis() throws {
-		let actual = refSettings.resolveVariables(in: "prefix $VALUE suffix")
-		let expected = "prefix Value suffix"
+		let actual = refSettings.resolveVariables(in: "prefix $VALUE_1 suffix")
+		let expected = "prefix Value 1 suffix"
 		XCTAssertEqual(actual, expected)
 	}
 	
