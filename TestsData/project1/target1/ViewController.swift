12431766f99475f14e9317ab8100dd9c26d268d5
Frizlab <fload@me.com>
2020-09-17 12:18:03 +0200

    Add special strings file case (more for LocMapper, but 🤷‍♂️)

--- TestsData/project1/target1/ViewController.swift
+++ TestsData/project1/target1/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: NSViewController {
 //		print("“\(Bundle.main.infoDictionary!["TEST_SPACES"]!)”")
 //		print("“\(Bundle.main.infoDictionary!["TEST_INCLUDES"]!)”")
 //		print("“\(Bundle.main.infoDictionary!["TEST_INCLUDES_2"]!)”")
+		print(NSLocalizedString("key_whose_value_is_the_key", value: "not in strings", comment: "yes, this is valid 🤦‍♂️"))
 		for (k, v) in Bundle.main.infoDictionary!.sorted(by: { $0.key < $1.key }) {
 			print("“\(k)” -> “\(v)”")
 		}
