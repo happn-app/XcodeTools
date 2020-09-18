fad5c4630af66c253bd2477642662f69b6280db1
Frizlab <fload@me.com>
2020-09-18 08:23:46 +0200

    Add a test for all Xcode projects

---
+++ Tests/XcodeProjKitTests/TestAllProjects.swift
@@ -0,0 +1,32 @@
+import Foundation
+import XCTest
+
+@testable import XcodeProjKit
+
+
+
+class TestAllProjects : XCTestCase {
+	
+	let testProjectsURL = URL(fileURLWithPath: #file, isDirectory: false).deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent().appendingPathComponent("TestsData").appendingPathComponent("projects")
+	
+	func testReserialization() throws {
+		let fm = FileManager.default
+		guard let de = fm.enumerator(atPath: testProjectsURL.path) else {
+			throw XcodeProjKitError(message: "Cannot get dir enumerator at path \(testProjectsURL.path)")
+		}
+		
+		while let f = de.nextObject() as! String? {
+			guard f.hasSuffix(".xcodeproj") ||  f.hasSuffix(".xcodeproj/") else {
+				continue
+			}
+			
+			let xcodeprojURL = URL(fileURLWithPath: f, isDirectory: true, relativeTo: testProjectsURL)
+			print("Testing project at path \(xcodeprojURL.path)")
+			
+			let xcodeproj = try XcodeProj(xcodeprojURL: xcodeprojURL)
+			let originalContents = try Data(contentsOf: xcodeproj.pbxprojURL)
+			try XCTAssertEqual(originalContents, Data(xcodeproj.pbxproj.stringSerialization(projectName: xcodeproj.projectName).utf8))
+		}
+	}
+	
+}
