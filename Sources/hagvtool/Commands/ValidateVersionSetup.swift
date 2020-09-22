56947a228a39712665854927939f7afbefe467ee
Frizlab <fload@me.com>
2020-09-23 01:45:52 +0200

    Start implementing different output formats for version setup validation
    The text output format is not yet supported; others are.

--- Sources/hagvtool/Commands/ValidateVersionSetup.swift
+++ Sources/hagvtool/Commands/ValidateVersionSetup.swift
@@ -11,61 +11,43 @@ struct ValidateVersionSetup : ParsableCommand {
 	var hagvtoolOptions: Hagvtool.Options
 	
 	func run() throws {
-		var nErrors = 0
 		let xcodeproj = try XcodeProj(path: hagvtoolOptions.pathToXcodeproj, autodetectInFolderAtPath: ".")
 		let xcodeprojURL = xcodeproj.xcodeprojURL
 		
-		do {
-			var ok = true
-			print("")
-			print("*** Verifying versioning system")
-			try xcodeproj.iterateCombinedBuildSettingsOfTargets(matchingOptions: hagvtoolOptions){ target, targetName, configurationName, combinedBuildSettings in
-				let versioningSystem = combinedBuildSettings[BuildSettingKey(key: "VERSIONING_SYSTEM")]
-				if versioningSystem != "apple-generic" {
-					ok = false
-					nErrors += 1
-					print("   -> Unexpected versioning system “\(versioningSystem)” for target “\(targetName)” and configuration “\(configurationName)”")
-				}
-			}
-			if ok {
-				print("-> OK")
+		let messages = try xcodeproj.iterateCombinedBuildSettingsOfTargets(matchingOptions: hagvtoolOptions){ target, targetName, configurationName, combinedBuildSettings -> [Output.DiagnosticMessage] in
+			let plistMessages: [Output.DiagnosticMessage?]
+			if let plist = try combinedBuildSettings.infoPlistRaw(xcodeprojURL: xcodeprojURL) {
+				let bundleVersion = plist["CFBundleVersion"] as? String
+				let bundleVersionMessage = Output.DiagnosticMessage(messageType: .invalidCFBundleVersionInPlist, value: bundleVersion, targetName: targetName, configurationName: configurationName)
+				
+				let shortVersionString = plist["CFBundleShortVersionString"] as? String
+				let shortVersionStringMessage = Output.DiagnosticMessage(messageType: .invalidCFBundleShortVersionStringInPlist, value: shortVersionString, targetName: targetName, configurationName: configurationName)
+				
+				plistMessages = [
+					(bundleVersion      != "$(CURRENT_PROJECT_VERSION)" ? bundleVersionMessage      : nil),
+					(shortVersionString != "$(MARKETING_VERSION)"       ? shortVersionStringMessage : nil)
+				]
 			} else {
-				print("-> FAIL")
-				print("The versioning system should be set to “apple-generic” for all targets, though in practice not setting this build setting will not change much.")
+				plistMessages = []
 			}
+			
+			let versioningSystem = combinedBuildSettings[BuildSettingKey(key: "VERSIONING_SYSTEM")]
+			let versioningSystemMessage = Output.DiagnosticMessage(messageType: .invalidVersioningSystem, value: versioningSystem, targetName: targetName, configurationName: configurationName)
+			
+			let nonPlistMessages = [
+				(versioningSystem != "apple-generic" ? versioningSystemMessage : nil)
+			]
+			
+			return (nonPlistMessages + plistMessages).compactMap{ $0 }
+		}.flatMap{ $0 }
+		
+		let output = Output(messages: messages)
+		try Hagvtool.printOutput(output, format: hagvtoolOptions.outputFormat)
+		
+		if !output.messages.isEmpty {
+			throw ExitCode(1)
 		}
 		
-		do {
-			var ok = true
-			print("")
-			print("*** Verifying sanity of current project version and marketing version in Info.plist")
-			try xcodeproj.iterateCombinedBuildSettingsOfTargets(matchingOptions: hagvtoolOptions){ target, targetName, configurationName, combinedBuildSettings in
-				guard let plist = try combinedBuildSettings.infoPlistRaw(xcodeprojURL: xcodeprojURL) else {
-					return
-				}
-				
-				let versionString = plist["CFBundleVersion"]
-				if versionString as? String != "$(CURRENT_PROJECT_VERSION)" {
-					ok = false
-					nErrors += 1
-					print("   -> Unexpected CFBundleVersion value “\(versionString ?? "<no value>")” in plist file for target “\(targetName)” and configuration “\(configurationName)”")
-				}
-				
-				let shortVersionString = plist["CFBundleShortVersionString"]
-				if shortVersionString as? String != "$(MARKETING_VERSION)" {
-					ok = false
-					nErrors += 1
-					print("   -> Unexpected CFBundleShortVersionString value “\(shortVersionString ?? "<no value>")” in plist file for target “\(targetName)” and configuration “\(configurationName)”")
-				}
-			}
-			if ok {
-				print("-> OK")
-			} else {
-				print("-> FAIL")
-				print("The CFBundleVersion value should be set to “$(CURRENT_PROJECT_VERSION)” and the CFBundleShortVersionString should be set to “$(MARKETING_VERSION)”.")
-				print("Of course, the actual versions should be set in the build settings (either directly in the project or using an xcconfig file).")
-			}
-		}
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "CURRENT_PROJECT_VERSION")] } })
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "DYLIB_CURRENT_VERSION")] } })
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "DYLIB_COMPATIBILITY_VERSION")] } })
@@ -75,8 +57,68 @@ struct ValidateVersionSetup : ParsableCommand {
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "VERSION_INFO_FILE")] } })
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "VERSION_INFO_PREFIX")] } })
 //		try print(combinedBuildSettings.mapValues{ try $0.mapValues{ try $0[BuildSettingKey(serializedKey: "VERSION_INFO_SUFFIX")] } })
+	}
+	
+	private struct Output : Encodable, CustomStringConvertible {
 		
-		throw ExitCode(nErrors > 0 ? 1 : 0)
+		struct DiagnosticMessage : Encodable {
+			
+			enum MessageType : String, Encodable {
+				
+				case invalidVersioningSystem = "invalid-versioning-system"
+				case invalidCFBundleVersionInPlist = "invalid-CFBundleVersion-in-plist"
+				case invalidCFBundleShortVersionStringInPlist = "invalid-CFBundleShortVersionString-in-plist"
+				
+			}
+			
+			var messageType: MessageType
+			var value: String?
+			
+			var targetName: String
+			var configurationName: String
+			
+		}
+		
+		var messages: [DiagnosticMessage]
+		
+		var description: String {
+//			print("*** Verifying versioning system")
+//			print("   -> Unexpected versioning system “\(versioningSystem)” for target “\(targetName)” and configuration “\(configurationName)”")
+//			if ok {
+//				print("-> OK")
+//			} else {
+//				print("-> FAIL")
+//				print("The versioning system should be set to “apple-generic” for all targets, though in practice not setting this build setting will not change much.")
+//			}
+//
+//			print("*** Verifying sanity of current project version and marketing version in Info.plist")
+//			print("   -> Unexpected CFBundleVersion value “\(versionString ?? "<no value>")” in plist file for target “\(targetName)” and configuration “\(configurationName)”")
+//			print("   -> Unexpected CFBundleShortVersionString value “\(shortVersionString ?? "<no value>")” in plist file for target “\(targetName)” and configuration “\(configurationName)”")
+//			if ok {
+//				print("-> OK")
+//			} else {
+//				print("-> FAIL")
+//				print("The CFBundleVersion value should be set to “$(CURRENT_PROJECT_VERSION)” and the CFBundleShortVersionString should be set to “$(MARKETING_VERSION)”.")
+//				print("Of course, the actual versions should be set in the build settings (either directly in the project or using an xcconfig file).")
+//			}
+			return ""
+		}
+		
+		init(messages m: [DiagnosticMessage]) {
+			messages = m.sorted{ m1, m2 in
+				if m1.messageType.rawValue < m2.messageType.rawValue {return true}
+				if m1.messageType.rawValue > m2.messageType.rawValue {return false}
+				
+				if m1.targetName < m2.targetName {return true}
+				if m1.targetName > m2.targetName {return false}
+				
+				if m1.configurationName < m2.configurationName {return true}
+				if m1.configurationName > m2.configurationName {return false}
+				
+				return true
+			}
+		}
+		
 	}
 	
 }
