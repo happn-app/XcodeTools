74a8f86af9b607e66c8f4667da0725723ca5ff81
Frizlab <fload@me.com>
2021-03-17 13:29:30 +0200

    Remove an unused variable

--- Sources/XcodeProjKit/ModelSingleton.swift
+++ Sources/XcodeProjKit/ModelSingleton.swift
@@ -5,8 +5,6 @@ import Foundation
 
 struct ModelSingleton {
 	
-	let syncQueue = DispatchQueue(label: "com.happn.XcodeTools.ModelSingletonSync")
-	
 	static var model: NSManagedObjectModel? = {
 		return Bundle.module.url(forResource: "PBXModel", withExtension: "momd").flatMap{ NSManagedObjectModel(contentsOf: $0) }
 	}()
