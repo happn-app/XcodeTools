4bc333b7b3a95261f9dd8ef9393d813dcd3f8087
Frizlab <fload@me.com>
2021-03-20 21:42:03 +0200

    Add empty shell for xct-build

--- Package.swift
+++ Package.swift
@@ -9,6 +9,10 @@ let package = Package(
 	],
 	products: [
 		.library(name: "XcodeProjKit", targets: ["XcodeProjKit"]),
+		
+		.library(name: "libxct", targets: ["libxct"]),
+		.executable(name: "xct", targets: ["xct"]),
+		.executable(name: "xct-build", targets: ["xct-build"]),
 		.executable(name: "xct-versions", targets: ["xct-versions"]),
 		
 		/* Obsolete; kept for backwards-compatibility. Will be removed. */
@@ -41,6 +45,8 @@ let package = Package(
 		
 		.target(name: "xct-build", dependencies: [
 			.product(name: "ArgumentParser", package: "swift-argument-parser"),
+			.product(name: "CLTLogger",      package: "clt-logger"),
+			.product(name: "Logging",        package: "swift-log"),
 			"XcodeProjKit"
 		]),
 		
--- Sources/xct-build/main.swift
+++ Sources/xct-build/main.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+import ArgumentParser
+import CLTLogger
+import Logging
+
+
+
+struct XctBuild : ParsableCommand {
+	
+	static var configuration = CommandConfiguration(
+		abstract: "Build an Xcode project",
+		discussion: "Hopefully, the options supported by this tool are easier to understand than xcodebuild’s."
+	)
+	
+	func run() throws {
+		LoggingSystem.bootstrap{ _ in CLTLogger(messageTerminator: "\n") }
+		let logger = Logger(label: "main")
+		
+		
+	}
+	
+}
+
+XctBuild.main()
