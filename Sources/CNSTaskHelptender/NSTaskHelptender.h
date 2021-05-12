c81e536d2b917c3a0833fe39eddb7531d97e8b5d
Frizlab <fload@me.com>
2021-05-12 20:06:28 +0200

    Add a typealias to help readability of the NSTask extender

--- Sources/CNSTaskHelptender/NSTaskHelptender.h
+++ Sources/CNSTaskHelptender/NSTaskHelptender.h
@@ -6,9 +6,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/* We declare a typealias to the function signature, not the block type directly
+ * for elegancy: https://news.ycombinator.com/item?id=13437182 */
+typedef void XCTTaskTerminationSignature(NSTask * _Nonnull);
+
 @protocol XCTTaskExtender <HPNExtender>
 
-@property(readonly) void (^additionalCompletionHandler)(NSTask * _Nonnull);
+@property(readonly) XCTTaskTerminationSignature ^additionalCompletionHandler;
 
 @end
 
--- Sources/CNSTaskHelptender/NSTaskHelptender.m
+++ Sources/CNSTaskHelptender/NSTaskHelptender.m
@@ -23,40 +23,44 @@ static char PUBLIC_TERMINATION_HANDLER_KEY;
 	[helptender resetTerminationHandler];
 }
 
-- (nullable void (^)(NSTask *))publicTerminationHandler
+- (nullable XCTTaskTerminationSignature ^)publicTerminationHandler
 {
 	return objc_getAssociatedObject(self, &PUBLIC_TERMINATION_HANDLER_KEY);
 }
 
-- (void)setPublicTerminationHandler:(nullable void (^)(NSTask * _Nonnull))terminationHandler
+- (void)setPublicTerminationHandler:(nullable XCTTaskTerminationSignature ^)terminationHandler
 {
 	objc_setAssociatedObject(self, &PUBLIC_TERMINATION_HANDLER_KEY, terminationHandler, OBJC_ASSOCIATION_COPY);
 }
 
-- (void)setTerminationHandler:(nullable void (^)(NSTask * _Nonnull))terminationHandler
+- (void)setTerminationHandler:(nullable XCTTaskTerminationSignature ^)terminationHandler
 {
 	[self setPublicTerminationHandler:terminationHandler];
 }
 
 - (void)overrideTerminationHandler
 {
-	void (^currentTerminationHandler)(NSTask *) = ((void (^(*)(id, SEL))(NSTask *))HPN_HELPTENDER_CALL_SUPER_NO_ARGS_WITH_SEL_NAME(XCTTaskHelptender, terminationHandler));
+	/* For the fun, below is the declaration without the
+	 * XCTTaskTerminationSignature typealias:
+	 *    void (^currentTerminationHandler)(NSTask *) = ((void (^(*)(id, SEL))(NSTask *))HPN_HELPTENDER_CALL_SUPER_NO_ARGS_WITH_SEL_NAME(XCTTaskHelptender, terminationHandler));
+	 */
+	XCTTaskTerminationSignature ^currentTerminationHandler = ((XCTTaskTerminationSignature ^(*)(id, SEL))HPN_HELPTENDER_CALL_SUPER_NO_ARGS_WITH_SEL_NAME(XCTTaskHelptender, terminationHandler));
 	[self setPublicTerminationHandler:currentTerminationHandler];
 	
-	void (^newTerminationHandler)(NSTask *) = ^(NSTask *task) {
+	XCTTaskTerminationSignature ^newTerminationHandler = ^(NSTask *task) {
 		for (id<XCTTaskExtender> extender in [self hpn_extendersConformingToProtocol:@protocol(XCTTaskExtender)]) {
-			void (^additionalTerminationHandler)(NSTask *) = [extender additionalCompletionHandler];
+			XCTTaskTerminationSignature ^additionalTerminationHandler = [extender additionalCompletionHandler];
 			if (additionalTerminationHandler != NULL) additionalTerminationHandler(self);
 		}
-		void (^terminationHandler)(NSTask *) = [self publicTerminationHandler];
+		XCTTaskTerminationSignature ^terminationHandler = [self publicTerminationHandler];
 		if (terminationHandler != NULL) terminationHandler(self);
 	};
-	((void (*)(id, SEL, void (^)(NSTask *)))HPN_HELPTENDER_CALL_SUPER_WITH_SEL_NAME(XCTTaskHelptender, setTerminationHandler:, newTerminationHandler));
+	((void (*)(id, SEL, XCTTaskTerminationSignature ^))HPN_HELPTENDER_CALL_SUPER_WITH_SEL_NAME(XCTTaskHelptender, setTerminationHandler:, newTerminationHandler));
 }
 
 - (void)resetTerminationHandler
 {
-	((void (*)(id, SEL, void (^)(NSTask *)))HPN_HELPTENDER_CALL_SUPER_WITH_SEL_NAME(XCTTaskHelptender, setTerminationHandler:, self.publicTerminationHandler));
+	((void (*)(id, SEL, XCTTaskTerminationSignature ^))HPN_HELPTENDER_CALL_SUPER_WITH_SEL_NAME(XCTTaskHelptender, setTerminationHandler:, self.publicTerminationHandler));
 	[self setPublicTerminationHandler:nil];
 }
 
