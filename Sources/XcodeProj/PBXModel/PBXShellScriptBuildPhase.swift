3ad41f1ef022c888a3a73a10316650a1cc817abc
Frizlab <fload@me.com>
2021-05-22 21:52:00 +0200

    Make input and output paths optional in PBXShellScriptBuildPhase

--- Sources/XcodeProj/PBXModel/PBXShellScriptBuildPhase.swift
+++ Sources/XcodeProj/PBXModel/PBXShellScriptBuildPhase.swift
@@ -9,10 +9,10 @@ public class PBXShellScriptBuildPhase : PBXBuildPhase {
 	open override func fillValues(rawObject: [String : Any], rawObjects: [String : [String : Any]], context: NSManagedObjectContext, decodedObjects: inout [String : PBXObject]) throws {
 		try super.fillValues(rawObject: rawObject, rawObjects: rawObjects, context: context, decodedObjects: &decodedObjects)
 		
-		inputPaths = try rawObject.getForParse("inputPaths", xcID)
+		inputPaths = try rawObject.getIfExistsForParse("inputPaths", xcID)
 		inputFileListPaths = try rawObject.getIfExistsForParse("inputFileListPaths", xcID)
 		
-		outputPaths = try rawObject.getForParse("outputPaths", xcID)
+		outputPaths = try rawObject.getIfExistsForParse("outputPaths", xcID)
 		outputFileListPaths = try rawObject.getIfExistsForParse("outputFileListPaths", xcID)
 		
 		shellPath = try rawObject.getForParse("shellPath", xcID)
@@ -30,10 +30,10 @@ public class PBXShellScriptBuildPhase : PBXBuildPhase {
 		var mySerialization = [String: Any]()
 		if let v = showEnvVarsInLog?.boolValue {mySerialization["showEnvVarsInLog"] = v ? "1" : "0"}
 		if let v = alwaysOutOfDate?.boolValue  {mySerialization["alwaysOutOfDate"] = v ? "1" : "0"}
+		if let v = inputPaths                  {mySerialization["inputPaths"] = v}
+		if let v = outputPaths                 {mySerialization["outputPaths"] = v}
 		if let v = inputFileListPaths          {mySerialization["inputFileListPaths"] = v}
 		if let v = outputFileListPaths         {mySerialization["outputFileListPaths"] = v}
-		mySerialization["inputPaths"]  = try getInputPaths()
-		mySerialization["outputPaths"] = try getOutputPaths()
 		mySerialization["shellPath"]   = try getShellPath()
 		mySerialization["shellScript"] = try getShellScript()
 		
