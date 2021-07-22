d8bf3cd75e374f65b790b86ced8dbf2af6643a9c
Frizlab <fload@me.com>
2021-07-22 19:43:41 +0200

    Update debug completion script

--- Scripts/debug-completion.swift
+++ Scripts/debug-completion.swift
@@ -13,6 +13,29 @@ print("""
 \(gray)# Not mandatory, but you’ll probably want that,\(reset)
 \(gray)# especially in bash while there is the bash completion script bug\(reset)
 \(gray)# See https://github.com/apple/swift-argument-parser/pull/323\(reset)
+XCT_DIR=`ls -d "$HOME/Library/Developer/Xcode/DerivedData"/XcodeTools-*/"Build/Products/Debug"` \(gray)# We might be able to find better than this…\(reset)
+export PATH="$XCT_DIR:$PATH" \(gray)# But check first XCT_DIR only contains one path!\(reset)
+
+\(redBold)zsh\(reset):
+\(gray)# Once\(reset)
+mkdir -p .build/completion_zsh
+fpath=("$(pwd)/.build/completion_zsh" "${fpath[@]}")
+\(gray)# After each xct build\(reset)
+xct --generate-completion-script zsh >.build/completion_zsh/_xct && unfunction _xct && compinit -D
+
+\(redBold)bash\(reset):
+\(gray)# Once\(reset)
+mkdir -p .build/completion_bash
+\(gray)# After each xct build\(reset)
+xct --generate-completion-script bash >.build/completion_bash/xct && . .build/completion_bash/xct
+""")
+
+/* When we’ll be able to build xct via the command line directly
+print("""
+\(redBold)All shells\(reset):
+\(gray)# Not mandatory, but you’ll probably want that,\(reset)
+\(gray)# especially in bash while there is the bash completion script bug\(reset)
+\(gray)# See https://github.com/apple/swift-argument-parser/pull/323\(reset)
 export PATH="$(pwd)/.build/debug:$PATH"
 
 \(redBold)zsh\(reset):
@@ -27,4 +50,4 @@ swift run xct --generate-completion-script zsh >.build/completion_zsh/_xct && un
 mkdir -p .build/completion_bash
 \(gray)# After each xct build\(reset)
 swift run xct --generate-completion-script bash >.build/completion_bash/xct && . .build/completion_bash/xct
-""")
+""") */
