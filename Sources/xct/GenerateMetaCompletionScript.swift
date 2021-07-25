a2c77dac08cf552c2a36562953e1fd32cb1f5bde
Frizlab <fload@me.com>
2021-07-25 17:20:57 +0200

    Add basic completion script from autogenerated ones

--- Sources/xct/GenerateMetaCompletionScript.swift
+++ Sources/xct/GenerateMetaCompletionScript.swift
@@ -23,7 +23,71 @@ struct GenerateMetaCompletionScript : ParsableCommand {
 	var shellName = Shell.zsh
 	
 	func run() throws {
-		print("yellow for \(shellName)")
+		switch shellName {
+			case .zsh:  print(Self.zshCompletion)
+			case .bash: print(Self.bashCompletion)
+		}
 	}
+	
+	private static let zshCompletion = """
+		#compdef xct
+		local context state state_descr line
+		_xct_commandname=$words[1]
+		typeset -A opt_args
+		
+		_xct() {
+			integer ret=1
+			local -a args
+			args+=(
+				'--exec-path[Set the path to the core xct programs.]:exec-path:_files -/'
+				'-C[Change working directory before calling the tool.]:path:_files -/'
+				':tool-name:{_custom_completion $_xct_commandname ---completion  -- toolName $words}'
+				':tool-arguments:{_custom_completion $_xct_commandname ---completion  -- toolArguments $words}'
+				'(-h --help)'{-h,--help}'[Print help information.]'
+			)
+			_arguments -w -s -S $args[@] && ret=0
+			
+			return ret
+		}
+		
+		
+		_custom_completion() {
+			local completions=("${(@f)$($*)}")
+			_describe '' completions
+		}
+		
+		_xct
+		"""
+	
+	private static let bashCompletion = """
+		#!/bin/bash
+		
+		_xct() {
+			cur="${COMP_WORDS[COMP_CWORD]}"
+			prev="${COMP_WORDS[COMP_CWORD-1]}"
+			COMPREPLY=()
+			opts="--exec-path -C -h --help"
+			opts="$opts $(xct ---completion  -- toolName "$COMP_WORDS")"
+			opts="$opts $(xct ---completion  -- toolArguments "$COMP_WORDS")"
+			if [[ $COMP_CWORD == "1" ]]; then
+				COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+				return
+			fi
+			case $prev in
+				--exec-path)
+					COMPREPLY=( $(compgen -d -- "$cur") )
+					return
+				;;
+				-C)
+					COMPREPLY=( $(compgen -d -- "$cur") )
+					return
+				;;
+			esac
+			COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+		}
+		
+		
+		complete -F _xct xct
+		"""
 	
 }
