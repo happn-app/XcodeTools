6c04e6e05b3cb808164e941913ed4a4012241bd5
Frizlab <fload@me.com>
2021-08-10 23:30:35 +0200

    Fix a typo

--- Sources/xct/GenerateMetaCompletionScript.swift
+++ Sources/xct/GenerateMetaCompletionScript.swift
@@ -9,7 +9,7 @@ import ArgumentParser
 struct GenerateMetaCompletionScript : ParsableCommand {
 	
 	static var configuration = CommandConfiguration(
-		abstract: "Generate the completion script for xct. We use another algorithm than ArgumentParser’s, but I did not find a way to override the completion script from ArgumentParser, so I create a new command."
+		abstract: "Generate the completion script for xct. We use another algorithm than ArgumentParser’s, but I did not find a way to override the completion script from ArgumentParser, so I created a new command."
 	)
 	
 	enum Shell : String, CaseIterable, ExpressibleByArgument {
