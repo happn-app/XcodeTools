eb3880619b5d918bb570f66c31d78ce10337079c
Frizlab <fload@me.com>
2021-08-18 11:31:05 +0200

    Reindent Process+Utils w/ Xcode 13

--- Sources/XcodeTools/Process+Utils.swift
+++ Sources/XcodeTools/Process+Utils.swift
@@ -31,7 +31,7 @@ class XcodeToolsProcessExtender : NSObject, XCTTaskExtender {
 extension Process {
 	
 	/**
-	How to redirect stdout and stderr? */
+	 How to redirect stdout and stderr? */
 	public enum RedirectMode {
 		case none
 		case toNull
@@ -40,66 +40,64 @@ extension Process {
 	}
 	
 	/**
-	Spawns a process, and streams all of its outputs (stdout, stderr and all
-	additional output file descriptors) in the handler (line by line, each line
-	containing the newline char, a valid newline is only "\n", but line might not
-	end with a newline if last line of the output).
-	
-	The process will be launched in its own PGID. Which means, if your process is
-	launched in a Terminal, then you spawn a process using this method, then the
-	user types Ctrl-C, your process will be killed, but the process you launched
-	won’t be.
-	
-	However, you have an option to forward some signals to the processes you
-	spawn using this method.
-	
-	IMHO the signal forwarding method, though a bit more complex (in this case, a
-	lot of the complexity is hidden by this method), is better than using the
-	same PGID than the parent for the child. In a shell, if a long running
-	process is launched from a bash script, and said bash script is killed using
-	a signal (but from another process sending a signal, not from the tty), the
-	child won’t be killed! Using signal forwarding, it will.
-	
-	Some interesting links:
-	- [The TTY demystified](http://www.linusakesson.net/programming/tty/)
-	- [SIGTTIN / SIGTTOU Deep Dive](http://curiousthing.org/sigttin-sigttou-deep-dive-linux)
-	- [Swift Process class source code](https://github.com/apple/swift-corelibs-foundation/blob/swift-5.3.3-RELEASE/Sources/Foundation/Process.swift)
-	
-	A dummy § for Xcode.
-	
-	- Important: For technical reasons (and design choice), if file descriptors
-	to send is not empty, the process will be launched _via_ `xct`.
-	
-	- Note: We use `Process` to spawn the process. This is why the process is
-	launched in its own PGID and why we have to use `xct` to launch it to be able
-	to pass other file descriptors than stdin/stdout/stderr to it.
-	
-	One day we might rewrite this function using `posix_spawn` directly…
-	
-	- Note: On Linux, the PGID stuff is not true up to Swift 5.3.3 (currently in
-	production!) It is true on the `main` branch though (2021-04-01).
-	
-	- Important: The handler will not be called on the calling thread (so you can
-	`waitUntilExit` on the `Process` and still receive the stream “live”).
-	
-	- Important: All of the `additionalOutputFileDescriptors` are closed when the
-	end of their respective stream are reached (i.e. the function takes ownership
-	of the file descriptors). Maybe later we’ll add an option not to close at end
-	of the stream.
-	
-	- Parameter fileDescriptorsToSend: The file descriptors (other than `stdin`,
-	`stdout` and `stderr`, which are handled and differently) to clone in the
-	child process. The **value** is the file descriptor to clone (from the parent
-	process to the child), the key is the descriptor you’ll get in the child
-	process.
-	- Parameter additionalOutputFileDescriptors: Additional output file
-	descriptors to stream from the process. Usually used with
-	`fileDescriptorsToSend` (you open a socket, give the write fd in fds to
-	clone, and the read fd to additional output fds). **Important**: The function
-	takes ownership of these file descriptors, i.e. it closes them when the end
-	of their respective streams is reached.
-	- Returns: The _started_ `Process` object that was created and a dispatch
-	group you can wait on to be sure the end of the streams was reached. */
+	 Spawns a process, and streams all of its outputs (stdout, stderr and all
+	 additional output file descriptors) in the handler (line by line, each line
+	 containing the newline char, a valid newline is only "\n", but line might
+	 not end with a newline if last line of the output).
+	 
+	 The process will be launched in its own PGID. Which means, if your process
+	 is launched in a Terminal, then you spawn a process using this method, then
+	 the user types Ctrl-C, your process will be killed, but the process you
+	 launched won’t be.
+	 
+	 However, you have an option to forward some signals to the processes you
+	 spawn using this method.
+	 
+	 IMHO the signal forwarding method, though a bit more complex (in this case,
+	 a lot of the complexity is hidden by this method), is better than using the
+	 same PGID than the parent for the child. In a shell, if a long running
+	 process is launched from a bash script, and said bash script is killed using
+	 a signal (but from another process sending a signal, not from the tty), the
+	 child won’t be killed! Using signal forwarding, it will.
+	 
+	 Some interesting links:
+	 - [The TTY demystified](http://www.linusakesson.net/programming/tty/)
+	 - [SIGTTIN / SIGTTOU Deep Dive](http://curiousthing.org/sigttin-sigttou-deep-dive-linux)
+	 - [Swift Process class source code](https://github.com/apple/swift-corelibs-foundation/blob/swift-5.3.3-RELEASE/Sources/Foundation/Process.swift)
+	 
+	 - Important: For technical reasons (and design choice), if file descriptors
+	 to send is not empty, the process will be launched _via_ `xct`.
+	 
+	 - Note: We use `Process` to spawn the process. This is why the process is
+	 launched in its own PGID and why we have to use `xct` to launch it to be
+	 able to pass other file descriptors than stdin/stdout/stderr to it.
+	 
+	 One day we might rewrite this function using `posix_spawn` directly…
+	 
+	 - Note: On Linux, the PGID stuff is not true up to Swift 5.3.3 (currently in
+	 production!) It is true on the `main` branch though (2021-04-01).
+	 
+	 - Important: The handler will not be called on the calling thread (so you
+	 can `waitUntilExit` on the `Process` and still receive the stream “live”).
+	 
+	 - Important: All of the `additionalOutputFileDescriptors` are closed when
+	 the end of their respective stream are reached (i.e. the function takes
+	 ownership of the file descriptors). Maybe later we’ll add an option not to
+	 close at end of the stream.
+	 
+	 - Parameter fileDescriptorsToSend: The file descriptors (other than `stdin`,
+	 `stdout` and `stderr`, which are handled and differently) to clone in the
+	 child process. The **value** is the file descriptor to clone (from the
+	 parent process to the child), the key is the descriptor you’ll get in the
+	 child process.
+	 - Parameter additionalOutputFileDescriptors: Additional output file
+	 descriptors to stream from the process. Usually used with
+	 `fileDescriptorsToSend` (you open a socket, give the write fd in fds to
+	 clone, and the read fd to additional output fds). **Important**: The
+	 function takes ownership of these file descriptors, i.e. it closes them when
+	 the end of their respective streams is reached.
+	 - Returns: The _started_ `Process` object that was created and a dispatch
+	 group you can wait on to be sure the end of the streams was reached. */
 	public static func spawnedAndStreamedProcess(
 		_ executable: String, args: [String] = [],
 		stdin: FileDescriptor? = FileDescriptor.standardInput,
@@ -110,11 +108,11 @@ extension Process {
 		signalsToForward: Set<Signal> = Signal.toForwardToSubprocesses,
 		outputHandler: @escaping (_ line: String, _ sourceFd: FileDescriptor) -> Void
 	) throws -> (Process, DispatchGroup) {
-		#if canImport(eXtenderZ)
+#if canImport(eXtenderZ)
 		let p = Process()
-		#else
+#else
 		let p = XcodeToolsProcess()
-		#endif
+#endif
 		
 		var fdToCloseInCaseOfError = Set<FileDescriptor>()
 		var mustCallTerminationHandlerInCaseOfError = false
@@ -138,7 +136,7 @@ extension Process {
 			case .toFd(let fd): p.standardOutput = FileHandle(fileDescriptor: fd.rawValue)
 			case .capture:
 				/* The Pipe init is non-fallible, but can fail. So we cast the Pipe
-				 * returned by the init to a Pipe? */
+				 * returned by the init to a Pipe? */
 				guard let pipe = Pipe() as Pipe? else {
 					try cleanupAndThrow(XcodeToolsError.internalError("Cannot open Pipe for stdout"))
 				}
@@ -195,11 +193,11 @@ extension Process {
 				try cleanupAndThrow(XcodeToolsError.envVarXctExecPathNotSet)
 			}
 			/* The socket to send the fd. The tuple thingy _should_ be _in effect_
-			 * equivalent to the C version `int sv[2] = {-1, -1};`.
-			 * https://forums.swift.org/t/guarantee-in-memory-tuple-layout-or-dont/40122
-			 * Stride and alignment should be the equal for CInt.
-			 * Funnily, it seems to only work in debug compilation, not in release…
-			 * var sv: (CInt, CInt) = (-1, -1) */
+			 * equivalent to the C version `int sv[2] = {-1, -1};`.
+			 * https://forums.swift.org/t/guarantee-in-memory-tuple-layout-or-dont/40122
+			 * Stride and alignment should be the equal for CInt.
+			 * Funnily, it seems to only work in debug compilation, not in release…
+			 * var sv: (CInt, CInt) = (-1, -1) */
 			let sv = UnsafeMutablePointer<CInt>.allocate(capacity: 2)
 			sv.initialize(repeating: -1, count: 2)
 			defer {sv.deallocate()}
@@ -271,11 +269,11 @@ extension Process {
 				XcodeToolsConfig.logger?.error("Cannot unregister delayed sigaction: \(error)", metadata: ["signal": "\(signal)"])
 			}
 		}
-		#if canImport(eXtenderZ)
+#if canImport(eXtenderZ)
 		p.hpn_add(XcodeToolsProcessExtender(terminationHandler))
-		#else
+#else
 		p.privateTerminationHandler = terminationHandler
-		#endif
+#endif
 		
 		XcodeToolsConfig.logger?.info("Launching process \(executable)\(fileDescriptorsToSend.isEmpty ? "" : " through xct")")
 		mustCallTerminationHandlerInCaseOfError = true
@@ -295,11 +293,11 @@ extension Process {
 	}
 	
 	/**
-	Exactly the same as `spawnedAndStreamedProcess`, but the process and the
-	streamed file descriptors are waited on and you get the termination status
-	and reason on return.
-	
-	- Returns: The exit status of the process and its termination reason. */
+	 Exactly the same as `spawnedAndStreamedProcess`, but the process and the
+	 streamed file descriptors are waited on and you get the termination status
+	 and reason on return.
+	 
+	 - Returns: The exit status of the process and its termination reason. */
 	public static func spawnAndStream(
 		_ executable: String, args: [String] = [],
 		stdin: FileDescriptor? = FileDescriptor.standardInput,
@@ -353,10 +351,10 @@ extension Process {
 			let toRead = Int(estimatedBytesAvailable * 2 + 1)
 			XcodeToolsConfig.logger?.trace("Reading around \(toRead) bytes from \(streamReader.sourceStream)")
 			/* We do not need to check the number of bytes actually read. If EOF
-			 * was reached (nothing was read), the stream reader will remember it,
-			 * and the readLine method will properly return nil without even trying
-			 * to read from the stream. Which matters, because we forbid the reader
-			 * from reading from the underlying stream (except in this read). */
+			 * was reached (nothing was read), the stream reader will remember it,
+			 * and the readLine method will properly return nil without even trying
+			 * to read from the stream. Which matters, because we forbid the reader
+			 * from reading from the underlying stream (except in this read). */
 			_ = try streamReader.readStreamInBuffer(size: toRead, allowMoreThanOneRead: false, bypassUnderlyingStreamReadSizeLimit: true)
 			
 			while let (lineData, eolData) = try streamReader.readLine() {
@@ -381,7 +379,7 @@ extension Process {
 		} catch {
 			XcodeToolsConfig.logger?.warning("Error reading from \(streamReader.sourceStream): \(error)")
 			/* We stop everything at first error. Most likely the error is bad fd
-			 * because process exited and we were too long to read from the stream. */
+			 * because process exited and we were too long to read from the stream. */
 			streamSource.cancel()
 		}
 	}
@@ -435,8 +433,8 @@ extension Process {
 
 #if !canImport(eXtenderZ)
 /**
-A subclass of Process whose termination handler is overridden, in order for
-XcodeTools to set its own termination handler and still let clients use it. */
+ A subclass of Process whose termination handler is overridden, in order for
+ XcodeTools to set its own termination handler and still let clients use it. */
 private class XcodeToolsProcess : Process {
 	
 	var privateTerminationHandler: ((Process) -> Void)? {
@@ -458,8 +456,8 @@ private class XcodeToolsProcess : Process {
 	private var publicTerminationHandler: ((Process) -> Void)?
 	
 	/**
-	Sets super’s terminationHandler to nil if both private and public termination
-	handlers are nil, otherwise set it to call them. */
+	 Sets super’s terminationHandler to nil if both private and public
+	 termination handlers are nil, otherwise set it to call them. */
 	private func updateTerminationHandler() {
 		if privateTerminationHandler == nil && publicTerminationHandler == nil {
 			super.terminationHandler = nil
