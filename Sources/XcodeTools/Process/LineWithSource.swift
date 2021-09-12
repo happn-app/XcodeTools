3df04740f203459edd075b53a1af80d2a0799d44
Frizlab <fload@me.com>
2021-09-12 08:30:34 +0300

    Add default encoding for line with source functions

--- Sources/XcodeTools/Process/LineWithSource.swift
+++ Sources/XcodeTools/Process/LineWithSource.swift
@@ -11,23 +11,23 @@ public struct RawLineWithSource : Equatable, Hashable {
 	
 	public var fd: FileDescriptor
 	
-	public func strLineWithSource(encoding: String.Encoding) throws -> LineWithSource {
+	public func strLineWithSource(encoding: String.Encoding = .utf8) throws -> LineWithSource {
 		return try LineWithSource(line: strLine(encoding: encoding), eol: strEOL(encoding: encoding), fd: fd)
 	}
 	
-	public func strLine(encoding: String.Encoding) throws -> String {
+	public func strLine(encoding: String.Encoding = .utf8) throws -> String {
 		return try String(data: line, encoding: encoding).get(orThrow: Err.invalidDataEncoding(line))
 	}
 	
-	public func strEOL(encoding: String.Encoding) throws -> String {
+	public func strEOL(encoding: String.Encoding = .utf8) throws -> String {
 		return try String(data: eol, encoding: encoding).get(orThrow: Err.invalidDataEncoding(line))
 	}
 	
-	public func strLineOrHex(encoding: String.Encoding) -> String {
+	public func strLineOrHex(encoding: String.Encoding = .utf8) -> String {
 		return String(data: line, encoding: encoding) ?? line.reduce("", { $0 + String(format: "%02x", $1) })
 	}
 	
-	public func strEOLOrHex(encoding: String.Encoding) -> String {
+	public func strEOLOrHex(encoding: String.Encoding = .utf8) -> String {
 		return String(data: eol, encoding: encoding) ?? line.reduce("", { $0 + String(format: "%02x", $1) })
 	}
 	
