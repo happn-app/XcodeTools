97f63afea19e1d9275a180c2325042e809f4f21b
Frizlab <fload@me.com>
2022-01-16 11:05:38 +0100

    Add new method to iterate sources of a target, and SPM targets of an XcodeProj

--- Sources/XcodeProj/XcodeProj/XcodeProj+Files.swift
+++ Sources/XcodeProj/XcodeProj/XcodeProj+Files.swift
@@ -7,8 +7,22 @@ extension XcodeProj {
 	
 	public func iterateReferencedFiles(_ handler: (_ fileURL: URL, _ knownFileType: String?) -> Void) throws {
 		try managedObjectContext.performAndWait{
-			try unsafeIterateFileElementsForFiles(fileElements: pbxproj.rootObject.getMainGroup().getChildren(), handler)
+			try unsafeIterateReferencedFiles(handler)
 		}
+	}
+	
+	public func iterateSources(of targetName: String, _ handler: (_ fileURL: URL, _ knownFileType: String?) -> Void) throws {
+		try managedObjectContext.performAndWait{
+			for target in try pbxproj.rootObject.getTargets().filter({ try $0.getName() == targetName }) {
+				for buildPhase in try target.getBuildPhases().compactMap({ $0 as? PBXSourcesBuildPhase }) {
+					try unsafeIterateFileElementsForFiles(fileElements: buildPhase.getFiles().compactMap{ $0.fileRef }, handler)
+				}
+			}
+		}
+	}
+	
+	internal func unsafeIterateReferencedFiles(_ handler: (_ fileURL: URL, _ knownFileType: String?) -> Void) throws {
+		try unsafeIterateFileElementsForFiles(fileElements: pbxproj.rootObject.getMainGroup().getChildren(), handler)
 	}
 	
 	private func unsafeIterateFileElementsForFiles(fileElements: [PBXFileElement], _ handler: (_ fileURL: URL, _ knownFileType: String?) -> Void) throws {
--- Sources/XcodeProj/XcodeProj/XcodeProj+Packages.swift
+++ Sources/XcodeProj/XcodeProj/XcodeProj+Packages.swift
@@ -11,10 +11,38 @@ extension XcodeProj {
 	 Finds file reference of type “wrapper” in the project.
 	 If the wrapper is a valid SPM package (an ``SPMProj`` object can be created from the URL), it is passed to you in the handler. */
 	public func iterateSPMPackagesInReferencedFile(_ handler: (_ proj: SPMProj) -> Void) throws {
-		try iterateReferencedFiles{ url, type in
-			guard type == "wrapper" else {return}
+		try managedObjectContext.performAndWait{
+			try unsafeIterateSPMPackagesInReferencedFile(handler)
+		}
+	}
+	
+	public func iterateSPMTargets(of targetName: String, _ handler: (_ proj: SPMProj, _ target: SPMTarget) -> Void) throws {
+		try managedObjectContext.performAndWait{
+			let spmDependencies = try pbxproj.rootObject
+				.getTargets()
+				.filter({ try $0.getName() == targetName })
+				.flatMap{ try $0.getBuildPhases().compactMap{ $0 as? PBXFrameworksBuildPhase } }
+				.flatMap{ try $0.getFiles().compactMap{ try $0.productRef?.getProductName() } }
+			
+			let spmDependenciesSet = Set(spmDependencies)
+			try unsafeIterateSPMPackagesInReferencedFile(targetNameFilter: spmDependenciesSet, { spmProj in
+				spmProj.targets.filter{ spmDependenciesSet.contains($0.name) }.forEach{ handler(spmProj, $0) }
+			})
+		}
+	}
+	
+	internal func unsafeIterateSPMPackagesInReferencedFile(targetNameFilter: Set<String> = [], _ handler: (_ proj: SPMProj) -> Void) throws {
+		try unsafeIterateReferencedFiles{ url, type in
+			guard type == "wrapper" || type == "folder" else {return}
 			guard let spmProj = try? SPMProj(url: url) else {
-				Conf.logger?.info("Found invalid SPM project at path \(url.path) in project at path \(xcodeprojURL.path)")
+				if type == "wrapper" {
+					/* We only log for the wrapper type; it is normal for folders not to be SPM projects, but some are anyway (and Xcode forgets to update their last know type). */
+					Conf.logger?.info("Found invalid SPM project at path \(url.path) in project at path \(xcodeprojURL.path)")
+				}
+				return
+			}
+			guard targetNameFilter.isEmpty || spmProj.targets.map({ $0.name }).contains(where: { targetNameFilter.contains($0) }) else {
+				Conf.logger?.debug("Skipped SPM package by filter: \(url.path)")
 				return
 			}
 			handler(spmProj)
